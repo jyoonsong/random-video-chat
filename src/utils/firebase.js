57922a4d7db2db854f2ee430c7d0759d07121752
Jaeyoon Song <jyo3on@gmail.com>
2021-07-24 07:52:09 -0400

    firebase setup

---
+++ src/utils/firebase.js
@@ -0,0 +1,23 @@
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import firebase from "firebase/app";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const firebase_instance = firebase;
+export const firebase_auth = firebase.auth();
+export const firestore = firebase.firestore();
