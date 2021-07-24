0640fffd78aee0f089ad6130adb5a2307caeb7a0
Jaeyoon Song <jyo3on@gmail.com>
2021-07-24 07:54:56 -0400

    implement google login in Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,16 @@
 import React from "react";
 
+import { firebase_auth, firebase_instance } from "../utils/firebase";
+
 const Login = () => {
 
+    const handleClick = async () => {
+        const provider = new firebase_instance.auth.GoogleAuthProvider();
+        await firebase_auth.signInWithPopup(provider);
+    }
+
     return (
-        <button>Continue with Google</button>
+        <button onClick={handleClick}>Continue with Google</button>
     )
 }
 export default Login;
