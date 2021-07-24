0f3900ea09404c11a00e11b70b083b340d3b9c6f
Jaeyoon Song <jyo3on@gmail.com>
2021-07-24 08:25:27 -0400

    render different views depending on the current state

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Chat = ({ currentUser }) => {
 
-    console.log(currentUser);
+    const [state, setState] = useState("offline"); // offline, waiting, talking
+
+    let returnValue = <button>Start a random video chat</button>
+
+    if (state === "waiting") {
+        returnValue = <div>
+            Searching for your partner...<br/>
+            Please wait a minute<br/>
+            <button>Stop searching</button>
+        </div>
+    }
+    else if (state === "talking") {
+        returnValue = <div>Talking</div>
+    }
 
     return (
-        <button>Start a random video chat</button> 
+        <>
+        {returnValue}
+        </>
     )
 }
 
